e16a476bd1ddf5942962172e5bcd1b971e2dca91
Biruk Anley <birukanley2022@gmail.com>
2023-03-14 22:56:10 +0300

    add layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Head from 'next/head';
+import Navbar from './Navbar';
+import { Main } from 'next/document';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div>
+      <div className="layout">
+          <Head>
+           <title>Kibur E-commerce Store</title>
+          </Head>
+          <header>
+            <Navbar/>
+          </header> 
+          <main className='main-container'>
+              {children}
+          </main> 
+          <footer>
+            <Footer/>
+          </footer>
+
+      </div>
+    </div>
   )
 }
 
