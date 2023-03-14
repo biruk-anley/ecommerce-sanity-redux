2a92a6cb31882aaeac6c3b4f55e26a1242413e26
Biruk Anley <birukanley2022@gmail.com>
2023-03-14 21:31:29 +0300

    update

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -21,7 +21,7 @@ const HeroBanner = ({heroBanner}) => {
 
           </Link>
 
-          <div className="desc">
+          <div className="desc"> 
             <h5>Description</h5>
             <p>{heroBanner.desc}</p>
 
