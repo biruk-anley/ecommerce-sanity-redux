553cdd206ddc9277c930b209c21871a8209f337f
Biruk Anley <birukanley2022@gmail.com>
2023-03-17 22:38:43 +0300

    update layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Head from 'next/head';
+
 import Navbar from './Navbar';
-import { Main } from 'next/document';
 import Footer from './Footer';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div>
-      <div className="layout">
-          <Head>
-           <title>Kibur E-commerce Store</title>
-          </Head>
-          <header>
-            <Navbar/>
-          </header> 
-          <main className='main-container'>
-              {children}
-          </main> 
-          <footer>
-            <Footer/>
-          </footer>
-
-      </div>
+    <div className="layout">
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
