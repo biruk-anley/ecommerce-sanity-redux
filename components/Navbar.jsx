e74ca100a37636aa4dafcf2d808599743d899c95
Biruk Anley <birukanley2022@gmail.com>
2023-03-17 22:38:53 +0300

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai'
 
 import { Cart } from './';
+import { useStateContext} from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     <div className="navbar-container">
@@ -12,12 +14,12 @@ const Navbar = () => {
         <Link href="/">JSM Headphones</Link>
       </p>
 
-      <button type="button" className="cart-icon" >
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty"></span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
 
-      
+      {showCart && <Cart />}
     </div>
   )
 }
