669c3590ea36a249f290802c85658674a3d5425d
Biruk Anley <birukanley2022@gmail.com>
2023-03-18 22:08:30 +0300

    update index .js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
+    
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
